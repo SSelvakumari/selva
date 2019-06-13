ea4e024aa1bd8e75e78b0580881e435ea79a37b4
SSelvakumari <51673087+SSelvakumari@users.noreply.github.com>
2019-06-13 10:53:22 +0530

    Update primerange.py

--- code kata/primerange.py
+++ code kata/primerange.py
@@ -1,5 +1,5 @@
 lower,upper=map(int,input().split())
-for num in range(lower+1,upper - 1):  
+for num in range(lower+1,upper + 1):  
    if num > 1:  
        for i in range(2,num):  
            if (num % i) == 0:  
